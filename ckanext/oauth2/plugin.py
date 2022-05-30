5f129558546b0d9457dde50758e710e521ddbc89
Kittichai Pholnikorn <kittichaipholnikorn@Kittichais-MacBook-Pro-2.local>
2022-05-30 21:56:35 +0700

    fix bugs

--- ckanext/oauth2/plugin.py
+++ ckanext/oauth2/plugin.py
@@ -137,6 +137,7 @@ class OAuth2Plugin(plugins.SingletonPlugin):
 
         log.info('[OAuth2Plugin] environ: %s' % environ)
         log.info('[OAuth2Plugin] authorization_header: %s' % self.authorization_header)
+        log.info('[OAuth2Plugin] authorization_header: %s' % toolkit.request.headers)
         
         if self.authorization_header == "authorization":
             if apikey.startswith('Bearer '):
